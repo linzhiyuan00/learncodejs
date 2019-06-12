354fa5309ef946db172aef58838eb087c15dfe85
linzhiyuan00 <2032247695@qq.com>
2019-06-12 20:44:55 +0800

    mp-vue

---
+++ VUE-learn/vue-eleme-app/src/components/goods/goods.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="goods">
+      <div class="menu-wrapper" ref="menuWrapper">
+        <ul>
+          <li
+            v-for="(item,index) in goods"
+            :key="index"
+            class="menu-item"
+            :class="{'current' : currentIndex === index}"
+            @click="selectName(index,$event)"
+          >
+            <span class="text border-1px">
+              <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+              {{item.name}}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+export default {
+  name: "Goods",
+  data() {
+    return {
+      classMap: [],
+      goods: []
+    };
+  },
+  created() {
+    this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
+    this.$http
+      .get(
+        "https://www.easy-mock.com/mock/5ca466e113e4cf68f04a42fb/cat-eye/vue-ele"
+      )
+      .then(res => {
+        if (res.data.errno === 0) {
+          this.goods = res.data.data;
+          this.$nextTick(() => {
+            // 页面渲染完成才能完成
+            this._initScroll();
+          });
+        }
+      });
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, { click: true });
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+@import '../../common/stylus/mixin.styl'
+.goods {
+  display: flex;
+  position: absolute;
+  top: 174px;
+  bottom: 46px;
+  width: 100%;
+  overflow: hidden;
+
+  .menu-wrapper {
+    flex: 0 0 80px;
+    width: 80px;
+    background-color: #f3f5f7;
+
+    .menu-item {
+      display: table;
+      height: 54px;
+      width: 56px;
+      padding: 0 12px;
+      line-height: 14px;
+
+      &.current {
+        z-index: 10;
+        margin-top: -1px;
+        background-color: #fff;
+        font-weight: 700;
+      }
+
+      .text {
+        border-none();
+
+        .icon {
+          display: inline-block;
+          vertical-align: top;
+          width: 12px;
+          height: 12px;
+          margin-right: 2px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+
+          &.decrease {
+            bg-image('decrease_3');
+          }
+
+          &.discount {
+            bg-image('discount_3');
+          }
+
+          &.guarantee {
+            bg-image('guarantee_3');
+          }
+
+          &.invoice {
+            bg-image('invoice_3');
+          }
+
+          &.special {
+            bg-image('special_3');
+          }
+        }
+      }
+    }
+  }
+}
+</style>
