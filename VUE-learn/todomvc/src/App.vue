c108626d88322e39271957a3969bd964264575c4
linzhiyuan00 <2032247695@qq.com>
2019-06-15 20:48:57 +0800

    vuex

---
+++ VUE-learn/todomvc/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <counter />
+  </div>
+</template>
+
+<script>
+// 两个兄弟组件, 共享count 数据
+// count  不应该属于Counter组件私有，不应该属于 data(){}, 应该props   
+import Counter from './components/Counter';
+export default {
+  data() {
+    return{
+      count:0
+    }
+  },
+  methods: {},
+  components:{
+    'counter':Counter
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE-learn/todomvc/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Clicked: {{$store.state.count}} times, count is
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+// 让store 里的改变状态的方法， mapActions
+import { mapActions } from 'vuex';
+export default {
+  //自身的状态
+  props:['count'],
+  data () {
+    return {
+      
+    }
+  },
+  methods:mapActions([
+    'increment'
+  ]) 
+}
+</script>
+
+<style>
+
+</style>
