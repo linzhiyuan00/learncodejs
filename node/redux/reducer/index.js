1c07972232c2ddbe865ab9184839d9775df19f49
linzhiyuan00 <2032247695@qq.com>
2019-06-26 10:03:14 +0800

    redux

---
+++ node/redux/reducer/index.js
@@ -0,0 +1,9 @@
+const { combineReducers } = require('redux');
+const filmReducer = require('./filmReducer.js');
+const filmFilterReducer = require('./filmFilter.js')
+
+
+module.exports = combineReducers({
+  films:filmReducer,
+  filter:filmFilterReducer
+})
