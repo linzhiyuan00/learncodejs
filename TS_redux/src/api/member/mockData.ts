42d89a30d3daa689ecfaedff563a39159bc8a1db
linzhiyuan00 <2032247695@qq.com>
2019-07-01 21:02:13 +0800

    typescript-redux

---
+++ TS_redux/src/api/member/mockData.ts
@@ -0,0 +1,14 @@
+import { MemberEntity } from "../../model";
+
+export const members:MemberEntity[] = [
+{
+  id:5151222,
+  login:"hjfbhsfbj",
+  avatar_url:"https://avatars.githubusercontent.com/u/1457912?v=3"
+},
+{
+  id:16966493,
+  login:'sdsbfbywhde',
+  avatar_url:"https://avatars.githubusercontent.com/u/4374977?v=3"
+}
+]
