d914463fa8154c94efe8cd7a8f261f4cf25d8316
linzhiyuan00 <2032247695@qq.com>
2019-07-10 16:38:44 +0800

    ...

--- gobang/src/components/main.jsx
+++ gobang/src/components/main.jsx
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+
+
+class Main  extends Component {
+  state = {  }
+  render() { 
+      return ( 
+        <div className="main">
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+       );
+  }
+}
+
+export default Main;
