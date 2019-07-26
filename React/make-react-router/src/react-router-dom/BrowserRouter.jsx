f280ccb24a635d2be76ce44c69ac38cca255bf29
linzhiyuan00 <2032247695@qq.com>
2019-07-26 15:33:25 +0800

    react-router

---
+++ React/make-react-router/src/react-router-dom/BrowserRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Router } from '../react-router/index';
+import { createBrowserHistory as createHistory } from "history";
+
+class BrowserRouter extends React.Component {
+  history = createHistory(this.props);
+  render() {
+    console.log(this.history);
+    return (
+      <Router history = {this.history} children = {this.props.children}>
+      </Router>
+    )
+  }
+}
+
+export default BrowserRouter;
